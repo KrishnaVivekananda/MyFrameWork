019620b906081ff6f43abd2c7c0c26233f9406ad
krishna <krishna@jk-reddys-mac-mini.local>
2017-04-13 10:14:27 +0530

    commit

---
+++ MyFramework/AddingTwoNumbers.swift
@@ -0,0 +1,17 @@
+//
+//  AddingTwoNumbers.swift
+//  MyFramework
+//
+//  Created by krishna on 13/04/17.
+//  Copyright © 2017 colors. All rights reserved.
+//
+
+import UIKit
+
+class AddingTwoNumbers: NSObject {
+
+    public func addNumbers(_ a:Int,_ b:Int) -> Int{
+        let c = a+b
+        return c
+    }
+}
